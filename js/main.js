ae86110874826296d4d4d4e1daacbfebecda92a4
S3N1OR <126277562+S3N1OR@users.noreply.github.com>
2024-07-07 15:37:09 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,47 @@
+let itemCount = 4;
+const container = document.querySelector('.container');
+
+function createItem(){
+	for (let i = 0; i < itemCount; i++) {
+  		container.innerHTML = container.innerHTML + `
+  			<div id="item-${i}" class="item">
+				<div class="block">
+					<h2 class="title">Кол-во штук</h2>
+					<input name="count-all-pcs-${i}" data-persist="garlic" type="number" id="count-all-pcs-${i}" oninput="result(${i})">
+				</div>
+
+				<div class="block">
+					<h2 class="title">Кол-во штук в уп.</h2>
+					<input name="count-box-pcs-${i}" data-persist="garlic" type="number" id="count-box-pcs-${i}" oninput="result(${i})" value="12">
+				</div> 
+
+				<div class="block">
+					<h2 class="title">Кол-во уп. на паллете.</h2>
+					<input name="count-pallet-box-${i}" data-persist="garlic" type="number" id="count-pallet-box-${i}" oninput="result(${i})" value="65">
+				</div>
+
+				<div id="result-${i}" class="result">
+					Упаковок: 0<br>Паллет: 0
+				</div>
+			</div>
+  		`
+	};
+}
+
+window.addEventListener('load', () => {
+	createItem();
+	result();
+});
+
+function result(itemCount){
+	let count_all_pcs = document.getElementById(`count-all-pcs-${itemCount}`);
+	let count_box_pcs = document.getElementById(`count-box-pcs-${itemCount}`);
+	let count_pallet_box = document.getElementById(`count-pallet-box-${itemCount}`);
+
+	const result = document.querySelector(`#result-${itemCount}`);
+
+	let resultBox = Math.floor(count_all_pcs.value / count_box_pcs.value);
+	let resultPallet = Math.floor(resultBox / count_pallet_box.value);
+
+	result.innerHTML = `Упаковок: ${resultBox}<br>Паллет: ${resultPallet}`;
+}
